01e9b05f6e4b24657c957a983f040e411d43d1da
Alexey Nikitin <alexeyn@cqg.com>
2020-05-11 19:55:29 +0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+PROJECT_DIR=~/projects/manjaro-sysprep
 sudo systemctl start sshd
 sudo systemctl enable sshd
 
@@ -10,10 +11,14 @@ sudo pacman -S --noconfirm ansible git yay ansible-lint yamllint vim
 
 mkdir -p ~/projects
 
-git clone https://github.com/alxzoomer/manjaro-sysprep.git ~/projects/manjaro-sysprep
+if [[ -d "${PROJECT_DIR}" ]] ; then
+  cd ~/projects/manjaro-sysprep
+  git pull
+else
+  git clone https://github.com/alxzoomer/manjaro-sysprep.git ~/projects/manjaro-sysprep
+  cd ~/projects/manjaro-sysprep
+fi
 
 # git clone git@github.com:alxzoomer/manjaro-sysprep.git ~/projects/manjaro-sysprep
-
-cd ~/projects/manjaro-sysprep
 
 make init
