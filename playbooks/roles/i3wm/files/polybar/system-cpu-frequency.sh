ecc93c0c2044509dde4bacd339573c0a60f9115d
Alexey Nikitin <alexeyn@cqg.com>
2020-05-16 19:11:41 +0400

    Add cpu freq for polybar bottom bar

---
+++ playbooks/roles/i3wm/files/polybar/system-cpu-frequency.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# Source https://github.com/polybar/polybar-scripts/blob/master/polybar-scripts/system-cpu-frequency/system-cpu-frequency.sh
+c=0;t=0
+
+awk '/MHz/ {print $4}' < /proc/cpuinfo | (while read -r i
+do
+    t=$( echo "$t + $i" | bc )
+    c=$((c+1))
+done
+echo "scale=2; $t / $c / 1000" | bc | awk '{print " "$1" GHz"}')
